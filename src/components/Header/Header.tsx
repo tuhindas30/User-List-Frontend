cbbb514927d7ff2d1eeccf33335f84d647c01360
Tuhin Das <mailme.tuhindas@gmail.com>
2022-05-13 10:32:21 +0530

    ✨ feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import brandLogo from "../../assets/images/Golds_Gym_Logo.png";
+import adminAvatar from "../../assets/images/Admin_Avatar.jpg";
+import { ReactComponent as SearchIcon } from "../../assets/icons/SearchIcon.svg";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src={brandLogo} alt="Brand Logo" className={styles.brandLogoImg} />
+      <div className={styles.inputContainer}>
+        <input type="text" placeholder="Search here" className={styles.input} />
+        <SearchIcon className={styles.searchIcon} />
+      </div>
+      <div className={styles.adminUser}>
+        <div className={styles.adminDetails}>
+          <p>John Doe</p>
+          <p>Admin</p>
+        </div>
+        <div className={styles.avatarContainer}>
+          <img
+            src={adminAvatar}
+            alt="Admin Avatar"
+            className={styles.adminAvatarImg}
+          />
+          <span className={styles.onlineBadge} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
